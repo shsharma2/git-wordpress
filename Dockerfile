3ae5bbd7bd3d5c64c2b4c01f265b3957a177ac50
unknown <Administrator@covalience.int>
2023-11-06 15:50:23 +0530

    no-reason

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@
 FROM wordpress:latest
 
 # Copy your WordPress files into the container
-COPY . /var/www/html
+# COPY . /var/www/html
 
 # Copy your WordPress config file into the container
-COPY wp-config.php /var/www/html/wp-config.php
+# COPY wp-config.php /var/www/html/wp-config.php
 
 # Set the working directory
 WORKDIR /var/www/html/
--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,8 @@
  *
  * @package WordPress
  */
-
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'shivi-blog' );
@@ -29,7 +30,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'db' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
